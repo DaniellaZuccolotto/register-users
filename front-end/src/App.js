0656a7666864e5ca25accc4b8c943c48e9eea2c0
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-11-14 01:17:29 -0300

    feat: Fiz parte da aplicação front-end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import UsersProvider from './provider/UsersProvider';
+import FormPage from './pages/FormPage';
+import UsersPage from './pages/UsersPage';
+import DetailsPage from './pages/DetailsPage';
+import InitialPage from './pages/InitialPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <UsersProvider>
+    <Routes>
+      <Route exact path="/" element={ <InitialPage /> } />
+      <Route exact path="/register" element={ <FormPage /> } />
+      <Route exact path="/users" element={ <UsersPage /> } />
+      <Route exact path="/users/:id" element={ <DetailsPage /> } />
+    </Routes>
+  </UsersProvider>
   );
 }
 
 export default App;
+
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import './styles/main.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Create a root container React 18 https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
