858cc7cf5d3b443d662132f397d2031822c21fd5
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-11-14 16:53:29 -0300

    feat: fiz algumas alterações no config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -2,21 +2,20 @@ require('dotenv').config();
 
 const environment = process.env.NODE_ENV || "test";
 
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
+// const suffix = {
+//   prod: "",
+//   production: "",
+//   dev: "-dev",
+//   development: "-dev",
+//   test: "-test",
+// };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'users_onesight'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
